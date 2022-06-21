a2d4a699bfad7127fe9097020680f17390d3f5d3
IspecialQ <ivanspecial13@gmail.com>
2022-06-21 13:16:05 +0300

    Ready

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,32 @@
-import javax.xml.crypto.Data;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.nio.charset.StandardCharsets;
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Main {
-    public static final String fileName = "table.csv";
+    private final static String WeatherURL = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/295212";
+    private final static String API_KEY = "?apikey=lrOd49BBYaNRKk6KQCOD373sABUhkUGA";
     public static void main(String[] args) {
+        try {
+            URL weatherURL = new URL(WeatherURL + API_KEY);
+            HttpURLConnection urlConnection = (HttpURLConnection) weatherURL.openConnection();
+            if (urlConnection.getResponseCode() == 200) {
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
+                    StringBuilder responseContent = new StringBuilder();
+                    String line = "";
+                    while ((line = reader.readLine()) != null) {
+                        responseContent.append(line);
+                    }
+                    System.out.println(responseContent);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
 
-       try (FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
-           byte[] csvData = newData().toString().getBytes(StandardCharsets.UTF_8);
-           fileOutputStream.write(csvData);
-       } catch (Exception e) {
-           System.out.println(e.getMessage());
-       }
-
-       try (FileInputStream fileInputStream = new FileInputStream(fileName)) {
-           byte[] csvDate = new byte[fileInputStream.available()];
-           fileInputStream.read(csvDate);
-           String table = new String(csvDate);
-           System.out.println(table);
-       } catch (Exception e) {
-           System.out.println(e.getMessage());
-       }
-
-    }
-
-    public static AppData newData() {
-        AppData appData = new AppData();
-        String[] headers = new String[] {"Value 1", "Value 2", "Value 3"};
-        appData.init(headers);
-        return appData;
     }
 }
 
